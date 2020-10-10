58c4e87c03db103c81629d1662233b7f40a5e89b
q-ironman <63059289+q-ironman@users.noreply.github.com>
2020-10-10 14:46:18 +0300

    updated trim methods

--- StringMethods0/Program.cs
+++ StringMethods0/Program.cs
@@ -11,48 +11,138 @@ namespace StringMethods0
     {
         static void Main(string[] args)
         {
-            string word;
+            /*string word;string oldstr;string newstr;  
+
             
-
-            /*word = Console.ReadLine();
-            oldstr = Console.ReadLine();
-            newstr = Console.ReadLine();*/
-            //word = Console.ReadLine();
-
-
-            /* word = Console.ReadLine();
-             Console.WriteLine(MyTrim(word));
-            Console.WriteLine("Program bitti");*/
-            /*word = Console.ReadLine();
-            trmchr = Console.ReadLine();
-            Console.WriteLine(MyTrimchr(word,trmchr));*/
-            /*word = Console.ReadLine();
-            trmchr = Console.ReadLine();
-            Console.WriteLine(MyTrimStart(word, trmchr));*/
-            /*word = Console.ReadLine();
-            trmchr = Console.ReadLine();
-            Console.WriteLine(MyTrimEnd(word, trmchr));*/
-            /*word = Console.ReadLine();
-            Console.WriteLine(MyClone(word));*/
-            /*char oldchr; char newchr;
+            string array_size;int array_size_int;
+            Console.WriteLine("Kaç karakter silmek istiyorsunuz");
+            array_size = Console.ReadLine();
+            array_size_int = Convert.ToInt32(array_size);
+            char[] char2remove = new char[array_size_int];
+            Console.WriteLine("silmek istediğiniz karakterleri giriniz");
+            for(int i = 0; i < array_size_int; i++)
+            {
+                char2remove[i] = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
             word = Console.ReadLine();
-            Console.WriteLine("Değiştirmek istediğiniz karakteri giriniz:");
-            oldchr = Console.ReadKey().KeyChar;
-            Console.WriteLine("\nYerleştirmek istediğiniz karakteri giriniz:");
-            newchr = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            Console.WriteLine(MyReplaceChr(word, oldchr, newchr));*/
-            
+            Console.WriteLine(NewTrimStart(word, char2remove));*/
+            Console.WriteLine(NewTrimStart("***aaaaabcbbbba",'a','*'));
+            Console.WriteLine(NewTrimStart("    kjfjsdknsj    1"));
+            Console.WriteLine(NewTrimEnd("aaabbkmsfdklmaaabbbb",'a','b'));
+            Console.WriteLine(NewTrimEnd("          cdknfskdnfskn      "));
+            Console.WriteLine(NewTrim("aabbbhfjdfaabbjfddbbbbaa",'a','b'));
+            Console.WriteLine(NewTrim("    dsfnsjfsfn    "));
             Console.ReadLine();
 
         }
-        static string MyReplaceStr(string word,string oldstr,string newstr)
+        static string NewTrimStart(string word, params char[] charToRemove)
         {
             
             
+            int length_word=word.Length;
+            int char2remove_length = charToRemove.Length;
+            int counter = 0;
+            int counter1 = 0;
             
+            for (int i = 0; i < length_word; i++)
+            {
+                for (int j = 0; j < char2remove_length; j++)
+                {
+                    if (word[i] == charToRemove[j])
+                    {
+                        counter += 1;
+                        counter1 += 1;
+                    }
+                    
+
+                }
+                if (counter1 == 0)
+                {
+                    break;
+                }
+                counter1 = 0;
+            }
+            //Console.WriteLine(counter);
             
-            return null;
+            StringBuilder new_str = new StringBuilder();
+            for (int i = 0; i < length_word - counter; i++)
+            {
+
+                new_str.Append(word[i+counter]);
+            }
+            return new_str.ToString();
+        }
+
+        static string NewTrimStart(string word)
+        {
+            return NewTrimStart(word, ' ');
+        }
+
+        static string NewTrimEnd(string word, params char[] charToRemove)
+        {
+            return ReverseStr(NewTrimStart(ReverseStr(word), charToRemove));
+        }
+
+        static string NewTrimEnd(string word)
+        {
+            return NewTrimEnd(word, ' ');
+        }
+
+        static string NewTrim(string word, params char[] charToRemove)
+        {
+            return NewTrimEnd(NewTrimStart(word, charToRemove), charToRemove);
+        }
+
+        static string NewTrim(string word)
+        {
+            return NewTrim(word, ' ');
+        }
+
+        static string MyReplaceStr(string word,string oldstr,string newstr)
+        {
+            int length_word = word.Length;
+            int length_oldstr=oldstr.Length;
+            string tmp;
+            int a = 0;int d = 0; int z = 0;int y =0 ;
+            for(int i = 0; i < length_word; i++)
+            {
+                int k = i;
+                for(int j = 0;j<length_oldstr;j++)
+                {
+                    if(word[k]==oldstr[j])
+                    {
+                        a += 1;
+                        k += 1;
+                        y += 1;
+                        if (a == length_oldstr)
+                        {
+                            Console.WriteLine("we have a match");
+                            d += 1;
+                            //Console.WriteLine(d);
+
+                        }
+                        
+                    }
+                    
+
+                    
+                }
+
+                int pos = y - length_oldstr;
+                if (pos == 0)
+                {
+                    tmp = newstr + word.Substring(pos+length_word-length_oldstr);
+                    Console.WriteLine(tmp);
+                    z += 1;
+                }
+                
+            }
+           
+
+
+
+            return null ;
         }
         static char[] MyReplaceChr(string word,char oldchr,char newchr)
         {
@@ -77,160 +167,25 @@ namespace StringMethods0
             }
             return word1;
         }
-        static string MyTrim(string word)
-        {
-            string a;
-            int c = 0;
-            a = word[0].ToString();
-            int i = 0;
-            if ( a == " ")
-            {
-                for(i=0;i<word.Length;i++)
-                {
-                    string b;
-                    b = word[i].ToString();
-                    if (b == " ")
-                    {
-                        
-                        c += 1;
-                        Console.WriteLine(c);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                word = word.Remove(0, c);
-                return word;
-
-            }
-
-            else
-            {
-                return word;
-            }
-
-        }
-        static string MyTrimchr(string word,string chr)
-        {
-            string firstchr;string lastchr;
-            int c = 0;
-            int c1 = 0;
-            int length = word.Length;
-            firstchr = word[0].ToString();
-            lastchr = word[word.Length - 1].ToString();
-            if(firstchr == chr)
-            {
-                for (int i = 0; i < word.Length; i++)
-                {
-                    string b;
-                    b = word[i].ToString();
-                    if (b == chr)
-                    {
-
-                        c += 1;
-                        
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    
-                }
-                word = word.Remove(0, c);
-                
-            }
-            if(lastchr==chr)
-            {
-                word = ReverseStr(word);
-                
-                for (int i = 0; i < word.Length; i++)
-                {
-                    string b;
-                    b = word[i].ToString();
-                    if (b == chr)
-                    {
-
-                        c1 += 1;
-                        
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-                }
-                word = word.Remove(0, c1);
-                word = ReverseStr(word);
-
-            }
-            
-            else
-            {
-                return word;
-            }
-
-
-
-            return word;
-        }
+       
+        
         static string ReverseStr(string word)
         {
-            string  reversedStr ="";
+           
+            StringBuilder sb = new StringBuilder();
             for(int i = word.Length-1;i>=0;i--)
             {
-                reversedStr += word[i];
+                sb.Append(word[i]);
+                
             }
-            return reversedStr;
+            return sb.ToString();
         }
-        static string MyTrimStart(string word,string chr)
-        {
-            string a;
-            int c = 0;
-            a = word[0].ToString();
-            int i = 0;
-            if (a == chr)
-            {
-                for (i = 0; i < word.Length; i++)
-                {
-                    string b;
-                    b = word[i].ToString();
-                    if (b == chr)
-                    {
-
-                        c += 1;
-                        Console.WriteLine(c);
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                word = word.Remove(0, c);
-                return word;
-
-            }
-
-            else
-            {
-                return word;
-            }
-
-
-
-
-            
-        }
-        static string MyTrimEnd(string word,string chr)
-        {
-            word = ReverseStr(word);
-            word = MyTrimStart(word,chr);
-            word = ReverseStr(word);
-            return word;
-        }
+        
+        
         static string MyClone(string word)
         {
             return word;
         }
+        
     }
 }
